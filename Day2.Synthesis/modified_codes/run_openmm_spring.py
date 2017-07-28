cace91df8969521856d704f653717c3891a0f043
Lee-Ping Wang <leeping@campus-037-155.ucdavis.edu>
2017-07-27 18:26:06 -0700

    Added "Putting it all together"

---
+++ Day2.Synthesis/modified_codes/run_openmm_spring.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit
+from sys import stdout
+
+# Load the PDB file into an object
+pdb = app.PDBFile('trpcage.pdb')
+
+# Load the force field file into an object
+forcefield = app.ForceField('amber99sbildn.xml', 'tip3p.xml')
+
+# Create system object using information in the force field:
+# forcefield: contains parameters of interactions
+# topology: lists of atoms, residues, and bonds
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.PME, 
+    nonbondedCutoff=1.0*unit.nanometers, constraints=app.HBonds, rigidWater=True, 
+    ewaldErrorTolerance=0.0005)
+
+# Create a Langevin integrator for temperature control
+integrator = mm.LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds, 
+    2.0*unit.femtoseconds)
+
+# Add a Monte Carlo barostat to the system for pressure control
+barostat = mm.MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25)
+system.addForce(barostat)
+
+# Use the CPU platform
+platform = mm.Platform.getPlatformByName('CPU')
+
+### If you want to add any forces to your System or modify any
+### of the existing forces, you should do it here - after the
+### System has been created, but before the Simulation is created.
+
+frc = mm.CustomBondForce("0.5*k*(r-r0)^2")
+frc.addGlobalParameter("k", 1000)
+frc.addGlobalParameter("r0", 2)
+
+frc.addBond(56, 200)
+system.addForce(frc)
+
+# Create a Simulation object by putting together the objects above
+simulation = app.Simulation(pdb.topology, system, integrator, platform)
+
+# Set positions in the Simulation object
+simulation.context.setPositions(pdb.positions)
+
+# Minimize the energy of the system
+print('Minimizing...')
+print(simulation.context.getState(getEnergy=True).getPotentialEnergy())
+simulation.minimizeEnergy(maxIterations=20, tolerance=100)
+print(simulation.context.getState(getEnergy=True).getPotentialEnergy())
+
+# Initialize the random velocities of the system from a Maxwell-Boltzmann distribution
+simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
+
+# Add reporters to the simulation object, which do things at regular intervals
+# while the simulation is running.
+# This reporter creates a DCD trajectory file
+simulation.reporters.append(app.DCDReporter('trajectory-spr.dcd', 100))
+
+# This reporter prints information to the terminal
+simulation.reporters.append(app.StateDataReporter(stdout, 100, step=True, 
+    potentialEnergy=True, temperature=True, density=True, progress=True, 
+    remainingTime=True, speed=True, totalSteps=10000, separator='\t'))
+
+# Run the simulation itself
+print('Running Production...')
+simulation.step(10000)
+print('Done!')
